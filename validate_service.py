2e4f4fb53df7f70f2e6ecf7ea810d5d07937fe58
thomasyu888 <thomas.yu@sagebase.org>
2020-12-08 21:34:05 +0800

    Don't auto remove yet

--- validate_service.py
+++ validate_service.py
@@ -55,8 +55,8 @@ def main(args):
     try:
         service = client.containers.run("curlimages/curl:7.73.0", exec_cmd,
                                         name=f"{args.submissionid}_curl_1",
-                                        network="submission", stderr=True,
-                                        auto_remove=True)
+                                        network="submission", stderr=True)
+                                        #auto_remove=True)
         service_info = json.loads(service.decode("utf-8"))
         expected_service_keys = ['author', 'authorEmail', 'description',
                                  'license', 'name', 'repository', 'url',
@@ -91,8 +91,8 @@ def main(args):
         example_post = client.containers.run(
             "curlimages/curl:7.73.0", exec_cmd,
             name=f"{args.submissionid}_curl_2",
-            network="submission", stderr=True,
-            auto_remove=True
+            network="submission", stderr=True
+            # auto_remove=True
         )
     except Exception:
         invalid_reasons.append(
