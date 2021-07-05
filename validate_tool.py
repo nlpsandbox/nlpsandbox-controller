4494687ca2671ec243dc9e330800dd0d0e3f64b7
thomasyu888 <thomas.yu@sagebase.org>
2021-07-05 15:14:14 +0800

    Remove

--- validate_tool.py
+++ validate_tool.py
@@ -99,7 +99,7 @@ def main(args):
         invalid_reasons.append(
             "API api/v1/tool endpoint not implemented or implemented "
             "incorrectly. Make sure correct tool object is returned.\n"
-            f"Error: {output_dir}"
+            f"Error: {tempdir}"
         )
     remove_docker_container(f"{args.submissionid}_curl_1")
 
