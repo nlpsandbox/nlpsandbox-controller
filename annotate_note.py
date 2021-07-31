e72bc598c2ed33e740b50c85b64aa62360d66b25
thomasyu888 <thomas.yu@sagebase.org>
2021-07-31 10:34:18 +0800

    Get the note responsible for internal error

--- annotate_note.py
+++ annotate_note.py
@@ -140,7 +140,8 @@ def main(syn, args):
         annotations = json.loads(annotate_note.decode("utf-8"))
         # If annotation fails, raise an error
         if annotations.get("status") is not None:
-            raise ValueError(annotations)
+            raise ValueError(note)
+            #raise ValueError(annotations)
         remove_docker_container(curl_name)
 
         # with open("annotations.json", "r") as note_f:
